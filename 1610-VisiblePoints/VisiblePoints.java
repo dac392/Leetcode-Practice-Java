4725f6fbe5ffb6f582e8a8a180d76697ded60307
dac392 <dac392@scarletmail.rutgers.edu>
2022-05-28 02:43:34 -0400

    needs to add sliding window

--- 1610-VisiblePoints/VisiblePoints.java
+++ 1610-VisiblePoints/VisiblePoints.java
@@ -2,8 +2,20 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class VisiblePoints{
+
+	public int visiblePoints(ArrayList<ArrayList<Integer>> points, int angle, ArrayList<Integer> location){
+		int maxPoints = 0;
+		for(int d = 0; d < 360; d++){
+			int[] fov = new int[2];
+			fov[0] = d - angle/2;
+			fov[1] = d + angle/2;
+			// i should use a sliding window
+		}
+		return maxPoints;
+	}
 	public static void main(String[] args) {
 		if(args.length != 0){
 			try{
@@ -13,6 +25,7 @@ public class VisiblePoints{
 				ArrayList<ArrayList<Integer>> points = new ArrayList<>();
 				ArrayList<Integer> location = new ArrayList<>();
 				Scanner scanner = new Scanner(System.in);
+				VisiblePoints vp = new VisiblePoints();
 				// reading point data into "points"
 				String line;
 				while( (line = reader.readLine())!=null ){
@@ -23,10 +36,10 @@ public class VisiblePoints{
 					}
 					points.add(list);	
 				}
-				// getting degree
+				// getting angle
 				System.out.println("Your data:\t"+points.toString());
-				System.out.print("Degree:\t");
-				float degree = scanner.nextFloat();			// degree of vision
+				System.out.print("Angle:\t");
+				int angle = scanner.nextInt();			// angle of vision
 				// getting (starting)location
 				System.out.print("Starting position:\t");
 				String sss = scanner.next();
@@ -35,10 +48,11 @@ public class VisiblePoints{
 					location.add(Integer.parseInt(num));
 				}
 
-				System.out.println("echo: "+points.toString());
-				System.out.println("echo: "+degree);
-				System.out.println("echo: "+location.toString());
+				// System.out.println("echo: "+points.toString());
+				// System.out.println("echo: "+angle);
+				// System.out.println("echo: "+location.toString());
 
+				System.out.println(vp.visiblePoints(points, angle, location));
 
 			}catch(Exception e){
 				e.printStackTrace();
